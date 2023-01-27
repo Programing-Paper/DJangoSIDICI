0b21f7adaacc4a0007f16ea696b7d907523d0180
programing <112596532+Programing-Paper@users.noreply.github.com>
2023-01-27 18:02:12 -0500

    actualizacion
    Se agregaron los modelos.

---
+++ main/urls.py
@@ -0,0 +1,32 @@
+"""main URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from .views import inicio, login, informacion, recuperar, logout_user, error_404
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',login,name='login'),
+    path('inicio/', inicio, name="inicio"),
+    path('usuarios/', include('usuarios.urls')),
+    path('novedades/', include('novedades.urls')),
+    path('activos/', include('activos.urls')),
+    path('informes/', include('informes.urls')),
+    path('configuracion/', include('configuracion.urls')),
+    path('informacion/',informacion, name='informacion'),
+    path('recuperar/',recuperar, name='recuperar'),
+    path('logout/',logout_user, name='logout'),  
+]
